b23bd5e01783ea2a962b675f7029c022b1b22605
Veerapat Lumsiricharoenchoke <punveerapat@Veerapats-MacBook-Air.local>
2025-04-04 13:49:13 +0700

    feat: controllers/companies, auth

---
+++ controllers/auth.js
@@ -0,0 +1,103 @@
+const User = require("../models/User");
+
+const sendTokenResponse = (user, statusCode, res) => {
+//create token
+  const token = user.getSignedJwtToken();
+
+  const options = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true,
+  };
+
+  if (process.env.NODE_ENV === "production") {
+    options.secure = true;
+  }
+
+  res
+    .status(statusCode)
+    // .cookie("token", token, options)
+    .json({ success: true, _id:user._id, name: user.name, email:user.email, token });
+};
+
+// @desc    Register a user
+// @route   POST /api/v1/auth/register
+// @access  Public
+exports.register = async (req, res, next) => {
+  try {
+    const { name, email, password, role } = req.body;
+
+    //create user
+    const user = await User.create({
+      name,
+      email,
+      password,
+      role,
+    });
+
+    sendTokenResponse(user, 200, res);
+  } catch (err) {
+    res.status(400).json({ success: false });
+    console.log(err.stack);
+  }
+};
+
+// @desc    Login a user
+// @route   POST /api/v1/auth/login
+// @access  Public
+exports.login = async (req, res, next) => {
+  try {
+  const { email, password } = req.body;
+
+//validate
+  if (!email || !password) {
+    return res
+      .status(400)
+      .json({ success: false, msg: "Please provide an email and password" });
+  }
+//check for user
+  const user = await User.findOne({ email }).select("+password");
+
+  if (!user) {
+    return res
+      .status(401)
+      .json({ success: false, msg: "Invalid credentials" });
+  }
+
+//check if password match
+  const isMatch = await user.matchPassword(password);
+
+  if (!isMatch) {
+    return res
+      .status(401)
+      .json({ success: false, msg: "Invalid credentials" });
+  }
+
+    sendTokenResponse(user, 200, res);
+  } catch (err) {
+    res.status(401).json({ success: false, msg: "Cannot convert email or password to string" });
+  }
+};
+
+// @desc    Get current logged in user
+// @route   POST /api/v1/auth/me
+// @access  Private
+exports.getMe = async (req, res, next) => {
+  const user = await User.findById(req.user.id);
+    res.status(200).json({
+        success: true,
+        data: user
+    });
+}
+
+exports.logout  = async (req, res, next) => {
+  res.cookie("token", "none", {
+    expires: new Date(Date.now() + 10 * 1000),
+    httpOnly: true,
+  });
+  res.status(200).json({
+    success: true,
+    data: {},
+  });
+}
---
+++ controllers/companies.js
@@ -0,0 +1,134 @@
+const Company = require('../models/Company');
+const Booking = require('../models/Booking');
+
+exports.getCompanies = async (req, res) => {
+    try {
+        let query;
+
+        const reqQuery = { ...req.query };
+        const removeFields = ['select', 'sort', 'page', 'limit'];
+
+        removeFields.forEach((param) => delete reqQuery[param]);
+        console.log(reqQuery);
+
+        let queryStr = JSON.stringify(reqQuery);
+        queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, match => `$${match}`);
+
+        query = Hospital.find(JSON.parse(queryStr)).populate('bookings');
+
+        if (req.query.select) {
+            const fields = req.query.select.split(',').join(' ');
+            query = query.select(fields);
+        }
+
+        if (req.query.sort) {
+            const sortBy = req.query.sort.split(',').join(' ');
+            query = query.sort(sortBy);
+        } else {
+            query = query.sort('-createdAt');
+        }
+
+        const page = parseInt(req.query.page, 10) || 1;
+        const limit = parseInt(req.query.limit, 10) || 25;
+        const startIndex = (page - 1) * limit;
+        const endIndex = page * limit;
+        const total = await Company.countDocuments();
+
+        query = query.skip(startIndex).limit(limit);
+
+        const companies = await query;
+        const pagination = {};
+
+        if (endIndex < total) {
+            pagination.next = {
+                page: page + 1,
+                limit
+            };
+        }
+
+        if (startIndex > 0) {
+            pagination.prev = {
+                page: page - 1,
+                limit
+            };
+        }
+
+        res.status(200).json({
+            success: true,
+            count: companies.length,
+            data: companies
+        });
+
+    } catch (err) {
+        res.status(400).json({
+            success: false,
+        });
+    }
+};
+
+exports.getCompany=async (req,res,next) => {
+    try{
+        const company = await Company.findById(req.params.id);
+        if(!company){
+            return res.status(400).json({success:false});
+        }
+        res.status(200).json({
+            success:true, data:company
+        });
+    }catch(err){
+        res.status(400).json({success:false});
+    }
+};
+
+exports.createCompany=async (req,res,next) => {
+    const company =await Company.create(req.body);
+    res.status(201).json({
+        success:true, 
+        data:company
+    });
+};
+
+exports.updateCompany=async (req,res,next) => {
+    try{
+        const company = await Company.findByIdAndUpdate(req.params.id,req.body,{
+            new:true,
+            runValidators:true
+        });
+        if(!company){
+            return res.status(400).json({success:false});
+        }
+        res.status(200).json({
+            success:true, data:company
+        });
+    }catch(err){
+        res.status(400).json({success:false});
+    }
+};
+
+exports.deleteCompany = async (req, res, next) => {
+    try {
+      const company = await Company.findById(req.params.id);
+  
+      if (!company) {
+        return res.status(404).json({
+            success: false,
+            message: `Company not found with id of ${req.params.id}`
+        });
+    }
+  
+       // Delete all appointments related to this hospital
+       await Company.deleteMany({ company: req.params.id });
+  
+       // Delete the hospital itself
+       await Company.deleteOne({ _id: req.params.id });
+  
+      res.status(200).json({
+        success: true,
+        data: {},
+      });
+    } catch (err) {
+      res.status(400).json({
+        success: false,
+      });
+    }
+};
