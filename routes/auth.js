790223aeaf4f0ea283ac74545240ec29fcbba200
DyeNerd <punveerapat@hotmail.com>
2025-04-19 14:59:19 +0700

    fix: update profile endpoint

--- routes/auth.js
+++ routes/auth.js
@@ -8,6 +8,6 @@ router.post("/register", register);
 router.post("/login", login);
 router.get("/me", protect, getMe);
 router.get("/logout", logout);
-router.put("/updateProfile", protect, updateProfile);
+router.put("/", protect, updateProfile);
 
 module.exports = router;
